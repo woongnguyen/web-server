b860e3e59c6d584fc8a086629beb83470f71b902
quannm <quannm@bit-vietnam.com>
2019-12-26 17:01:24 +0700

    render handle bar

--- src/app.js
+++ src/app.js
@@ -1,30 +1,38 @@
 
 const path = require('path')
 const express = require('express')
+const hbs = require('hbs')
 
-const publicDirectoryPath = path.join(__dirname, '../public')
 const app = express()
 
+const publicDirectoryPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname,'../templates/partials')
+
 app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
+
 app.use(express.static(publicDirectoryPath))
 
 app.get('', (req, res) =>{
   res.render('index', {
     title: 'Weather app',
-    name:'Woong'
+    name:'quannm'
   })
 })
 
 app.get('/about', (req, res) =>{
   res.render('about', {
     title: 'About',
-    name:'Woong'
+    name:'quannm'
   })
 })
 
 app.get('/help', (req, res) =>{
   res.render('Help',{
     title:'Help',
+    name: 'quannm',
     contact:'woongnguyen.15@gmail.com'
   })
 })
@@ -36,6 +44,21 @@ app.get('/weather', (req, res) =>{
   })
 })
 
+app.get('/help/*', (req, res) =>{
+  res.render('404',{
+    title:'404',
+    name:'quannm',
+    errorMessage:'Help article not found'
+  })
+})
+
+app.get('*', (req, res) =>{
+  res.render('404',{
+    title:'404',
+    name:'quannm',
+    errorMessage:'404 not found'
+  })
+})
 app.listen(3000, () =>{
   console.log('Server is up on port 3000.')
 })
